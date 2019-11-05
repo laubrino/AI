ef06e553fca3e380b12a2a103ff996d566132188
Laubr, Tomas <tomas.laubr@dieboldnixdorf.com>
2019-11-05 17:58:14 +0100

    notify agent early

--- reversi/src/main/java/cz/laubrino/ai/reversi/Main.java
+++ reversi/src/main/java/cz/laubrino/ai/reversi/Main.java
@@ -25,9 +25,10 @@ public class Main {
         Policko currentColor;
         Agent currentAgent;
 
-        for (int n=0;n<100_000;n++) {
+        for (int n=0;n<1_000;n++) {
             environment.reset();
             currentColor = Policko.BLACK;
+            environment.notifyObservers(currentColor, new StepResult(new State(environment.getBoard()), 0f, false, StepResult.Status.CONTINUE));
 
             for (long i = 0; (i< Environment.BOARD_SIZE * Environment.BOARD_SIZE + 3) && !environment.isGameOver(); i++) {
                 if (currentColor == Policko.BLACK) {
