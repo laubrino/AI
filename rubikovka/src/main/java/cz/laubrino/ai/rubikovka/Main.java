cd4284808b4abecf15142c3f8e320b0a4463060e
Laubr, Tomas <tomas.laubr@dieboldnixdorf.com>
2019-12-03 17:15:26 +0100

    Rubik's cube: use chart observer

--- rubikovka/src/main/java/cz/laubrino/ai/rubikovka/Main.java
+++ rubikovka/src/main/java/cz/laubrino/ai/rubikovka/Main.java
@@ -2,10 +2,11 @@ package cz.laubrino.ai.rubikovka;
 
 import cz.laubrino.ai.framework.AgentConfiguration;
 import cz.laubrino.ai.framework.Processor;
+import cz.laubrino.ai.framework.observers.ChartObserver;
 import cz.laubrino.ai.framework.observers.SysoutObserver;
 
 public class Main {
-    private static final long EPISODES = 5_000;
+    private static final long EPISODES = 1_000_000;
     private static final int MAX_STEPS_PER_EPISODE = 1_000;
     private static final int TESTING_EPIZODES = 100;        // number of plays in each testing step during learning
     private static final float TESTING_FREQ = 0.001f;
@@ -15,8 +16,8 @@ public class Main {
         Processor<Action> processor = new Processor<>(new AgentConfiguration(0.5f, 0.9f, 0.99995f, 1f), RubikEnvironment::new,
                 EPISODES, MAX_STEPS_PER_EPISODE, TESTING_FREQ, TESTING_EPIZODES, Action.class);
 
-        SysoutObserver sysoutObserver = new SysoutObserver();
-        processor.addObserver(sysoutObserver);
+        processor.addObserver(new SysoutObserver());
+        processor.addObserver(new ChartObserver("Rubik's Cube RL"));
 
         processor.process();
     }
