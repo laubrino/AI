e607b90adee062bf43f035b4b257fcfe9516fef4
Laubr, Tomas <tomas.laubr@dieboldnixdorf.com>
2019-12-03 10:48:09 +0100

    Rubik's cube: use predefined observer for output

--- rubikovka/src/main/java/cz/laubrino/ai/rubikovka/Main.java
+++ rubikovka/src/main/java/cz/laubrino/ai/rubikovka/Main.java
@@ -2,9 +2,10 @@ package cz.laubrino.ai.rubikovka;
 
 import cz.laubrino.ai.framework.AgentConfiguration;
 import cz.laubrino.ai.framework.Processor;
+import cz.laubrino.ai.framework.observers.SysoutObserver;
 
 public class Main {
-    private static final long EPISODES = 5_000_000;
+    private static final long EPISODES = 5_000;
     private static final int MAX_STEPS_PER_EPISODE = 1_000;
     private static final int TESTING_EPIZODES = 100;        // number of plays in each testing step during learning
     private static final float TESTING_FREQ = 0.001f;
@@ -14,10 +15,9 @@ public class Main {
         Processor<Action> processor = new Processor<>(new AgentConfiguration(0.5f, 0.9f, 0.99995f, 1f), RubikEnvironment::new,
                 EPISODES, MAX_STEPS_PER_EPISODE, TESTING_FREQ, TESTING_EPIZODES, Action.class);
 
-        RubikObserver rubikObserver = new RubikObserver();
-        processor.addObserver(rubikObserver);
+        SysoutObserver sysoutObserver = new SysoutObserver();
+        processor.addObserver(sysoutObserver);
 
         processor.process();
-        rubikObserver.shutDown();
     }
 }
